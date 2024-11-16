826050546756005afb9bc6d17c84d99b909a95e1
Yu Jiaming <yuey9012@gmail.com>
2024-11-16 17:29:30 +0800

    small changes

--- commands/define.js
+++ commands/define.js
@@ -23,23 +23,42 @@ module.exports = {
         const word = options.getString('word');
         const geturl = "https://www.dictionary.com/browse/" + word;
 
-        console.log(geturl);
+        await interaction.deferReply();
         
-        //web scrap
-        const axiosResponse = await axios.request({
-            method: "GET",
-            url: geturl,
-            headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-            }
-        })
+        try {
+            //web scrap
+            const axiosResponse = await axios.request({
+                method: "GET",
+                url: geturl,
+                headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+                }
+            })
 
-        const $ = cheerio.load(axiosResponse.data);
+            const $ = cheerio.load(axiosResponse.data);
 
-        const htmlElement = $(".NZKOFkdkcvYgD3lqOIJw");
-        const definition = htmlElement.text();
+            const htmlElement = $(".NZKOFkdkcvYgD3lqOIJw");
 
-        await interaction.reply({ content: `${definition}` });
-        return;
+            let definition = "";
+            htmlElement.each((i, div) => {
+
+                let toadd = $(div).text();
+                if (toadd[0] === " ") {
+                    definition += "-";
+                };
+                definition += toadd;
+                definition += "\n";
+            });
+
+            // const definition = htmlElement.text();
+            await interaction.editReply({ content: `${word}:\n${definition}-# From <${geturl}>` });
+            return;
+
+        } catch (err) {
+            console.log(err);
+            await interaction.editReply({ content: `You gave a bad word.` });
+            return;
+
+        };
     }
 };
